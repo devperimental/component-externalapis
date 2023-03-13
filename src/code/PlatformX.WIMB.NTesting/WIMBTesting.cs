04413f9794e99015ddbd7d60359d948e64658d0b
Christian Farinella <christian.farinella@ai.astra.co.id>
2023-03-14 07:51:22 +1100

    fix: actions

--- src/code/PlatformX.WIMB.NTesting/WIMBTesting.cs
+++ src/code/PlatformX.WIMB.NTesting/WIMBTesting.cs
@@ -36,6 +36,7 @@ namespace PlatformX.WIMB.NTesting
                 UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
             };
 
+
             var responseData = "{ \"result\": { \"code\" : \"success\", \"message_code\" : \"success\", \"message\" : \"success\" }, \"parse\": { \"user_agent\" : \"internet explorer\" } }";
             var appLogger = new Mock<ILogger<HttpRequestHelper>>();
 
